b0b5bb9e7aee86ea1079f740944a3755346ae113
Kupperu <vuken30227@gmail.com>
2019-05-01 08:00:25 +0100

    Updated from computer. New functions added

--- Texteditor.hs
+++ Texteditor.hs
@@ -1,70 +1,9 @@
 import Data.List 
 import System.IO
+
 {-
-data TextEditor = TextEditor {
-    highlights :: String,
-    contents :: String,
-    clipboard :: String,
-    cursor :: Int
-
-} deriving Show
-
-initial = TextEditor {highlights = "", contents = "hello there", clipboard = "", cursor = length (contents initial) - 1}
-b = highlightLeftChar initial
-showContent :: TextEditor -> String
-showContent TextEditor{highlights = h, contents = c, clipboard = cb} =  c
-
-inputWord :: String -> TextEditor -> TextEditor
-inputWord userInput file =
-    file { contents = contents file ++ " " ++ userInput}
-
-inputWordInInit userInput =
-    initial { contents = contents initial ++ " " ++ userInput}
-
-
-update updatedText file = 
-    file { contents = updatedText }
-
-
-backSpace :: TextEditor -> TextEditor
-backSpace a = a { contents = init(showContent a)}
-
--- as-pattern
-moveCursorLeft (textEditor@TextEditor{cursor = cur}) = 
-    textEditor { cursor = if cur == 0 then cur else cur - 1 }
-
-
-moveCursorRight (textEditor@TextEditor{cursor = cur, contents = c}) = 
-    textEditor { cursor = 
-        if cur == length c 
-            then cur 
-            else cur + 1 }
-
-highlightLeftChar (textEditor@TextEditor{cursor = cur, highlights = h, contents = con}) = 
-    moveCursorLeft (textEditor { highlights = con !! cur : h})
-
-
-highlightRightChar (textEditor@TextEditor{cursor = cur, highlights = h, contents = con}) = 
-    moveCursorRight (textEditor { highlights = con !! cur : h})
-
-copy (textEditor@TextEditor{highlights = h}) = 
-    textEditor{clipboard = h}
-
-paste (textEditor@TextEditor{clipboard = cb, contents = con, cursor = cur}) = 
-    textEditor{contents = (take cur con) ++ cb ++ (drop cur con), highlights = ""}
-
-loadFile = do  
-    putStr "Input file name you want to load: "
-    fileName <- getLine --getLine to read what the user input is
-    if null fileName --Error handling to stop the user from trying to input nothing.
-        then return() 
-        else do
-            handle <- openFile fileName ReadMode
-            contents <- hGetContents handle --hGetContents is lazy, therefore it won't try to load the whole text file into memory. But only loads parts you need
-            putStrLn contents           
-            hClose handle
-
-
+    TODO: Load file
+        moveCursorLeftWord
 -}
 data TextEditor2 = TextEditor2 {
     highlights :: String,
@@ -73,24 +12,22 @@ data TextEditor2 = TextEditor2 {
     clipboard :: String
 } deriving Show
 
-init2 = TextEditor2 {highlights = "", leftOfCursor = "Sup Hello ", rightOfCursor = "World test", clipboard = ""}
-{-
-b = moveCursorLeft init2
-c = moveCursorLeft b
-d = moveCursorLeft c
-e = moveCursorLeft d
-f = moveCursorLeft e
-g = moveCursorLeft f
-z = moveCursorLeft g
-h = moveCursorRight init2
-i = moveCursorRight h
-j = moveCursorRight i
-k = moveCursorRight j
-l = moveCursorRight k
-m = moveCursorRight l
--}
+--init has some default text in the left set and the right set
+init2 :: TextEditor2
+init2 = TextEditor2 {highlights = "", leftOfCursor = "Lorem ipsum", rightOfCursor = " dolor sit", clipboard = ""}
+
+--Creates an empty TextEditor
+create :: TextEditor2
+create = TextEditor2 {highlights = "", leftOfCursor = "", rightOfCursor = "", clipboard = ""}
+
 inputWord :: [Char] -> TextEditor2 -> TextEditor2
 inputWord a (textEditor@TextEditor2{leftOfCursor = loc}) = textEditor{leftOfCursor = loc ++ a}
+
+showLeft :: TextEditor2 -> String
+showLeft TextEditor2 {leftOfCursor = loc}= loc
+
+showRight :: TextEditor2 -> String
+showRight TextEditor2 {rightOfCursor = roc}= roc
 
 moveCursorLeft :: TextEditor2 -> TextEditor2
 moveCursorLeft (textEditor@TextEditor2{leftOfCursor = loc, rightOfCursor = roc}) = textEditor{
@@ -112,17 +49,55 @@ moveCursorRight (textEditor@TextEditor2{leftOfCursor = loc, rightOfCursor = roc}
         else tail roc
 }
 
+moveCursorLeftWord :: TextEditor2 -> TextEditor2
+moveCursorLeftWord (textEditor@TextEditor2{leftOfCursor = loc, rightOfCursor = roc}) = textEditor{
+    leftOfCursor = if length roc == 0
+        then loc
+        else show(init(words loc)),
+    rightOfCursor = if length roc == 0
+        then []
+        else last(words loc) ++ roc
+}
+
+moveCursorStart :: TextEditor2 -> TextEditor2
+moveCursorStart (textEditor@TextEditor2{leftOfCursor = loc, rightOfCursor = roc}) = textEditor{
+    leftOfCursor = "",
+    rightOfCursor = loc ++ roc
+}
+
+moveCursorEnd :: TextEditor2 -> TextEditor2
+moveCursorEnd (textEditor@TextEditor2{leftOfCursor = loc, rightOfCursor = roc}) = textEditor {
+    leftOfCursor = loc ++ roc,
+    rightOfCursor = ""
+}
+
+moveCursorLeftAmount :: Int -> TextEditor2 -> TextEditor2
+moveCursorLeftAmount 0 (textEditor@TextEditor2{leftOfCursor = loc, rightOfCursor = roc}) = moveCursorLeft textEditor
+moveCursorLeftAmount x (textEditor@TextEditor2{leftOfCursor = loc, rightOfCursor = roc}) = moveCursorLeftAmount (x - 1) (moveCursorLeft textEditor)
+
+moveCursorRightAmount :: Int -> TextEditor2 -> TextEditor2
+moveCursorRightAmount 0 (textEditor@TextEditor2{leftOfCursor = loc, rightOfCursor = roc}) = moveCursorRight textEditor
+moveCursorRightAmount x (textEditor@TextEditor2{leftOfCursor = loc, rightOfCursor = roc}) = moveCursorRightAmount (x - 1) (moveCursorRight textEditor)
+
 highlightLeftChar :: TextEditor2 -> TextEditor2
-highlightLeftChar (textEditor@TextEditor2{leftOfCursor = loc, highlights = h}) = moveCursorLeft $ textEditor {highlights = last loc : h}
+highlightLeftChar (textEditor@TextEditor2{leftOfCursor = loc, highlights = h}) = moveCursorLeft $ textEditor {highlights = 
+    if length loc == 0
+        then h
+        else last loc : h
+}
 
 highlightRightChar :: TextEditor2 -> TextEditor2
-highlightRightChar (textEditor@TextEditor2{rightOfCursor = roc, highlights = h}) = moveCursorRight $ textEditor {highlights = h ++ [head roc]}
+highlightRightChar (textEditor@TextEditor2{rightOfCursor = roc, highlights = h}) = moveCursorRight $ textEditor {highlights = 
+    if length roc == 0    
+        then h
+        else h ++ [head roc]
+}
 
 highlightLeftSideCursor :: TextEditor2 -> TextEditor2
-highlightLeftSideCursor (textEditor@TextEditor2{leftOfCursor = loc}) = textEditor{highlights = loc}
+highlightLeftSideCursor (textEditor@TextEditor2{leftOfCursor = loc}) = moveCursorStart $ textEditor{highlights = loc}
 
 highlightRightSideCursor :: TextEditor2 -> TextEditor2
-highlightRightSideCursor (textEditor@TextEditor2{rightOfCursor = roc}) = textEditor{highlights = roc}
+highlightRightSideCursor (textEditor@TextEditor2{rightOfCursor = roc}) = moveCursorEnd $ textEditor{highlights = roc}
 
 highlightLeftWord :: TextEditor2 -> TextEditor2
 highlightLeftWord (textEditor@TextEditor2{leftOfCursor = loc}) = textEditor{highlights = last(words loc)}
@@ -130,12 +105,24 @@ highlightLeftWord (textEditor@TextEditor2{leftOfCursor = loc}) = textEditor{high
 highlightRightWord :: TextEditor2 -> TextEditor2
 highlightRightWord (textEditor@TextEditor2{rightOfCursor = roc}) = textEditor{highlights = head(words roc)}
 
-highlightLeftWords :: TextEditor2 -> Int -> TextEditor2
-highlighLeftWords textEditor2 0 = textEditor2
-highlightLeftWords textEditor numberOfRepeats = do
-    let a = highlightLeftWord textEditor
-    highlightLeftWords a (numberOfRepeats - 1)
+--Recursive highlighting characters
+highlightLeftAmount :: Int -> TextEditor2 -> TextEditor2
+highlightLeftAmount 0 (textEditor@TextEditor2{leftOfCursor = loc, highlights = h}) = moveCursorLeft $ highlightLeftChar textEditor
+highlightLeftAmount x (textEditor@TextEditor2{leftOfCursor = loc, highlights = h}) = moveCursorLeft (highlightLeftAmount (x - 1) (highlightLeftChar textEditor))
 
+highlightRightAmount :: Int -> TextEditor2 -> TextEditor2
+highlightRightAmount 0 (textEditor@TextEditor2{leftOfCursor = loc, highlights = h}) = moveCursorRight $ highlightRightChar textEditor
+highlightRightAmount x (textEditor@TextEditor2{leftOfCursor = loc, highlights = h}) = moveCursorRight (highlightRightAmount (x - 1) (highlightRightChar textEditor))
+
+--Recursive highlighting words
+highlightWordLeftAmount :: Int -> TextEditor2 -> TextEditor2
+highlightWordLeftAmount 0 (textEditor@TextEditor2{leftOfCursor = loc, highlights = h}) = moveCursorLeft $ highlightLeftWord textEditor
+highlightWordLeftAmount x (textEditor@TextEditor2{leftOfCursor = loc, highlights = h}) = moveCursorLeft (highlightWordLeftAmount (x - 1) (highlightLeftWord textEditor))
+
+highlightWordRightAmount :: Int -> TextEditor2 -> TextEditor2
+highlightWordRightAmount 0 (textEditor@TextEditor2{leftOfCursor = loc, highlights = h}) = moveCursorRight $ highlightRightWord textEditor
+highlightWordRightAmount x (textEditor@TextEditor2{leftOfCursor = loc, highlights = h}) = moveCursorRight (highlightWordRightAmount (x - 1) (highlightRightWord textEditor))
+    
 highlightClear :: TextEditor2 -> TextEditor2
 highlightClear clearing = clearing {highlights = ""}
 
@@ -145,7 +132,22 @@ copy (textEditor@TextEditor2{highlights = h}) = textEditor {clipboard = h}
 paste :: TextEditor2 -> TextEditor2
 paste (textEditor@TextEditor2{clipboard = c, leftOfCursor = loc}) = textEditor{leftOfCursor = loc ++ c, highlights = ""}
 
+--cut (textEditor@TextEditor2{highlights = h, leftOfCursor = loc, rightOfCursor = roc}) = textEditor{highlights = ""}
+backspace :: TextEditor2 -> TextEditor2
+backspace (textEditor@TextEditor2{leftOfCursor = loc}) = textEditor {leftOfCursor = init loc}
 
+loadFile :: FilePath -> IO() --FilePath is basically just a string
+loadFile fileName = do
+    handle <- openFile fileName ReadMode
+    contents <- hGetContents handle
+    putStrLn contents
+    hClose handle --Impure function. Can't get IO to String since IO is impure.
+
+saveFile :: String -> TextEditor2 -> IO ()
+saveFile fileName (textEditor@TextEditor2{leftOfCursor = loc, rightOfCursor = roc}) = do
+    let x = (showLeft textEditor) ++ (showRight textEditor)
+    writeFile fileName x
+    putStrLn "File saved"
 
 a = highlightRightChar init2
 b = highlightRightChar a
